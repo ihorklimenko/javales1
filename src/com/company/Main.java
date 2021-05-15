7213635b599edcc5c2662e75b51d7308ece7fa5f
ihorklimenko <ihorklymenko45@gmail.com>
2021-05-15 10:58:22 +0300

    comit5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,17 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hola");
+        System.out.println("hola");
+        System.out.println("hola");
+        System.out.println("hola");
+        System.out.println("hola");
+        System.out.println("hola");
+        System.out.println("hola");
+        System.out.println("hola");
+        System.out.println("hola");
+        System.out.println("hola");
+        int x = 1;
+        int y = 0;
+        System.out.println(x+y);
     }
 }
